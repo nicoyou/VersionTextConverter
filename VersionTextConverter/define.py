8b69c9fb7e3988543b5641b014a0d3d88f2cb373
にこよう <50352295+nicoyou@users.noreply.github.com>
2021-10-16 23:26:38 +0900

    1.0.0

---
+++ VersionTextConverter/define.py
@@ -0,0 +1,76 @@
+CHARACTER_ENCODING = "utf-8"
+HTML_TEMPLATE = '''<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>{title}</title>
+	<meta name="description" content="{description}">
+	<style type="text/css">
+		th {{
+			background: gainsboro;
+		}}
+
+		td.center {{
+			text-align: center;
+		}}
+
+		.text_box {{
+			position: relative;
+			margin: 0.5em 0.5em;
+			padding: 25px 10px 7px;
+			border: solid 2px #FFC107;
+		}}
+
+		.text_box .box-title {{
+			position: absolute;
+			display: inline-block;
+			top: -2px;
+			left: -2px;
+			padding: 0 9px;
+			height: 25px;
+			line-height: 25px;
+			font-size: 17px;
+			background: #FFC107;
+			color: #ffffff;
+			font-weight: bold;
+		}}
+
+		.text_box p {{
+			margin: 0;
+			padding: 0;
+		}}
+	</style>
+</head>
+
+<body>
+{body}</body>
+</html>'''
+
+TABLE_HTML = '''<table border="1" style="border-collapse: collapse;">
+	<thead>
+		<tr>
+			<th>アップデート</th>
+			<th>バージョン</th>
+			<th>リリース日</th>
+			<th>変更内容</th>
+		</tr>
+	</thead>
+	<tbody>
+{tbody}</tbody>
+</table>
+'''
+
+BOX_HTML = '''
+				<div class="text_box" style="border-color: {color}; background: {back_color};">
+					<span class="box-title", style="background: {color};">{title}</span>
+					<p>{text}
+					</p>
+				</div>'''
+
+HTML_ESCAPE_TABLE = [
+	["&", "&amp;"],
+	['"', "&quot;"],
+	["'", "&apos;"],
+	[">", "&gt;"],
+	["<", "&lt;"],
+]
---
+++ VersionTextConverter/version_text_converter.py
@@ -0,0 +1,87 @@
+import glob
+import os
+import re
+import define
+
+# バージョン情報のテキストを数字に変換する
+def version_text_to_num(version):
+	try:
+		reg = r'([0-9]*?)\.([0-9]*?)\.([0-9]*?)$'
+		result = re.match(reg, version)
+		return [int(result.group(1)), int(result.group(2)), int(result.group(3))]
+	except Exception:
+		return [0, 0, 0]
+	return
+
+# HTML記号をエスケープする
+def escape_html(text):
+	for row in define.HTML_ESCAPE_TABLE:
+		text = text.replace(row[0], row[1])
+	return text
+
+
+file_path_list = glob.glob("./*.txt")
+
+for file_path in file_path_list:
+	with open(file_path, "r", encoding=define.CHARACTER_ENCODING) as f:
+		text = f.read()
+		tbody = ""
+		combining_flag = False			# セル結合フラグ
+
+		reg_info = r'[\s\S]*?ver\.([\s\S]*?)\s\(([0-9/]{8,10})\)([\s\S]*?)\n([\s\S]*?)-{5,300}[\s\S]*?'
+		for row in re.finditer(reg_info, text, re.MULTILINE):
+			reg_title = r'.*?\[(.*?)\].*?'
+			reg_title_result = re.match(reg_title, row.group(3))
+			if reg_title_result:
+				update_title = reg_title_result.group(1)
+			else:
+				update_title = ""
+			#print(row.group(1))
+			#print(row.group(2))
+			#print(update_title)
+			#print(row.group(4).rstrip("\n"))
+			#print("#"*60)
+
+			tbody += "\t"*2 + "<tr>\n"
+			version_num_list = version_text_to_num(row.group(1))
+			if version_num_list[2] == 0:			# 新要素追加のアップデートなら
+				if not combining_flag:				# 結合されたセルでなければ通常通り処理する
+					tbody += "\t"*3 + '<td class="center">{}</td>\n'.format(update_title)
+				else:								# 結合されたセルの一番最初のバージョンなら更新タイトルを参照する
+					tbody = tbody.format(title=update_title)
+					combining_flag = False
+			else:									# バグ修正のアップデートなら
+				if not combining_flag:				# 一番上のバージョンにタイトルセルを結合する
+					tbody += "\t"*3 + '<td class="center" rowspan="{}">{}</td>\n'.format(version_num_list[2] + 1, "{title}")
+					combining_flag = True
+			tbody += "\t"*3 + '<td class="center">{}</td>\n'.format(row.group(1))			# バージョン
+			datetime_str = re.sub(r'^(.*?)/(.*?)/(.*?)$', r'\1年\2月\3日', row.group(2))
+			tbody += "\t"*3 + '<td class="center">{}</td>\n'.format(datetime_str)			# 日付
+			contents = row.group(4).rstrip("\n")							# 更新内容
+			contents = escape_html(contents)
+			contents_list = contents.split("\n\n", 1)						# 機能追加と不具合修正で分割する
+			for i in range(len(contents_list)):
+				contents_list[i] = contents_list[i].replace("\n", "<br>\n" + "\t"*6)
+			contents = define.BOX_HTML.format(title="機能追加", color="royalblue", back_color="aliceblue", text=contents_list[0])
+			if version_num_list[2] != 0:			# 不具合修正だけのバージョンなら最初の塊から不具合修正にする
+				contents = define.BOX_HTML.format(title="不具合修正と調整", color="orange", back_color="lightgoldenrodyellow", text=contents_list[0])
+			if len(contents_list) == 2:
+				contents += define.BOX_HTML.format(title="不具合修正と調整", color="orange", back_color="lightgoldenrodyellow", text=contents_list[1])
+			tbody += "\t"*3 + "<td>{}\n\t\t\t</td>\n".format(contents)
+			tbody += "\t"*2 + "</tr>\n"
+
+		
+		table = define.TABLE_HTML.format(tbody=tbody)
+		html_text = define.HTML_TEMPLATE.format(title="更新履歴", description="ツールの更新履歴", body=table)
+		out_file_path = os.path.splitext(file_path)[0] + ".html"
+		with open(out_file_path, "w", encoding=define.CHARACTER_ENCODING) as f:
+			f.write(html_text)
+		print("\n\n更新履歴をHTMLへ変換しました\n{}".format(out_file_path))
+
+
+
+'''     更新履歴
+----------------------------------------------------------------------------------------------------------
+ver.1.0.0 (2021/09/26)
+リリース
+'''
